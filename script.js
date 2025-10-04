17c23829b6316f08c4b3990357856f9357e32dc3
Hasibul Nihad <hasibulnihad@gmail.com>
2025-10-05 00:26:41 +0600

    up and down button, admission button add

--- script.js
+++ script.js
@@ -66,4 +66,13 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+            const topBtn = document.getElementById('topBtn');
+        window.onscroll = () => {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200)
+            topBtn.style.display = "block";
+        else
+            topBtn.style.display = "none";
+        };
+        topBtn.onclick = () => window.scrollTo({top:0, behavior:'smooth'});
+
 });
